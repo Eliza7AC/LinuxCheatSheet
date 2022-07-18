8cde5ada1091668504b0bf95bca33e1fc8264cee
slash <slash@slash.com>
2022-07-18 17:54:59 +0200

    NODEJS WORKS

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static('./dist/untitled16'));
+// app.use(express.static(__dirname + '/dist/untitled16'));
 
 app.get('/*', function(req,res){
   res.sendFile(
     path.join(__dirname+'/dist/untitled16/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8085);
